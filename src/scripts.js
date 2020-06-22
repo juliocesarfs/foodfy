3d952cf7b23d65078e8f57f310f34684fa9744f1
Crucciatus <juliocesarafs2@gmail.com>
2020-06-22 20:37:48 -0300

    modal overlay finished

--- src/scripts.js
+++ src/scripts.js
@@ -1,12 +1,21 @@
 const modalOverlay = document.querySelector('.modal-overlay');
-const recipes = document.querySelectorAll('.recipe-image');
+const recipes = document.querySelectorAll('.recipe');
 
 for (let recipe of recipes) {
   recipe.addEventListener('click', function(){
     modalOverlay.classList.add('active');
+
+    const recipeImg = recipe.getAttribute('id');
+    const recipeName = recipe.querySelector('h3').textContent;
+    const recipeAuthorName = recipe.querySelector('p').textContent;
+
+    modalOverlay.querySelector('h3').textContent = recipeName;
+    modalOverlay.querySelector('img').src = `../assets/${recipeImg}`;
+    modalOverlay.querySelector('p').textContent = recipeAuthorName;
   });
 }
 
 document.querySelector('.close-modal').addEventListener('click', function(){
   modalOverlay.classList.remove('active');
+  
 });
