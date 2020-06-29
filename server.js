539c66185321eca59416ea173a3670971f5bc40b
Crucciatus <juliocesarafs2@gmail.com>
2020-06-29 13:53:39 -0300

    Refatoring: paste structure change | create server.js with NodeJS and express | add nunjucks template engine

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+const server = express();
+
+server.use(express.static('public'));
+
+server.use(express.static('assets'));
+
+server.set('view engine', 'njk');
+
+nunjucks.configure('views', { 
+  express: server,
+  autoescape: false,
+  noCache: true
+});
+
+server.get('/', function(req, res) {
+  return res.render('index');
+});
+
+server.get('/about.njk', function(req, res) {
+  return res.render('about');
+});
+
+
+server.get('/recipes.njk', function(req, res) {
+  return res.render('recipes');
+});
+
+
+server.listen(5000, function() {
+  console.log('Server is running');
+})
